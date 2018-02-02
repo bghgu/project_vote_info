aeb69d188858eb0190a1060da0f3f4d9815c5717
daseal bae <ektmf1993@gmail.com>
2018-02-02 14:54:03 +0900

    vote percent

--- routes/realTime.js
+++ routes/realTime.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const jwt = require('../module/jwt.js');
 const db = require('../module/pool.js');
 
-router.post('/', async(req, res, next) => {
-
+//현재 시간 투표율
+router.get('/', async(req, res, next) => {
+    const query = 'select v.voteId, v.voteName, v.startTime, v.endTime, d.departmentName as target from VOTEINFO v join DEPARTMENT d on v.target = d.departmentId';
+    let data = await db.execute(query);
+    if(data.length != 0) {
+        res.status(200).send({
+            message : "SUCCESS",
+            data : data
+        });
+    }else {
+        res.status(400).send({
+            message : "FAIL"
+        });
+    }
 });
 
 module.exports = router;
--- routes/vote.js
+++ routes/vote.js
@@ -5,16 +5,50 @@ const db = require('../module/pool.js');
 
 //모든 투표 리스트
 router.get('/', async(req, res, next) => {
-    const query = 'select v.voteId, v.voteName, v.startTime, v.endTime, d.departmentName as target from VOTEINFO v join DEPARTMENT d on v.target = d.departmentId';
+    
+    const query = 'select v.voteId, v.voteName, v.startTime, v.endTime, d.departmentName as target, v.target from VOTEINFO v join DEPARTMENT d on v.target = d.departmentId';
+    let query2 = '';
+    let query3 = '';
+
     let data = await db.execute(query);
-    if(data.length != 0) {
-        res.status(200).send({
-            message : "SUCCESS",
-            data : data
+    
+    if(data.length == 0) {
+        res.status(400).send({
+            status : "FAIL",
+            data : null,
+            msg : "투표 리스트 조회 실패"
         });
     }else {
-        res.status(400).send({
-            message : "FAIL"
+        for(i = 0; i < data.length; i++) {
+            let value = data[i].target;
+            let all = 0;
+            let valued = 0;
+            if(value == 1) {
+                query2 = 'select count(*) as count from USER';
+                query3 = 'select count(*) as count from AUTH';
+                all = await db.execute(query2);
+                valued = await db.execute(query3);
+            }
+            else if(value % 10 == 0) {
+                query2 = 'select count(*) as count from USER where departmentId BETWEEN ? AND ?';
+                query3 = 'select count(*) as count from AUTH where departmentId BETWEEN ? AND ?';
+                all = await db.execute3(query2, value, value + 9);
+                valued = await db.execute3(query3, value, value + 9);
+            }
+            else {
+                query2 = 'select count(*) as count from USER where departmentId = ?';
+                query3 = 'select count(*) as count from AUTH where departmentId = ?';
+                all = await db.execute(query2, value);
+                valued = await db.execute(query3, value);
+            }
+            data[i].allVoter = all[0].count;
+            data[i].valuedVoter = valued[0].count;
+            data[i].vetePercent = ((valued[0].count/all[0].count)*100).toFixed(2); + "%";
+        }
+        res.status(200).send({
+            status : "SUCCESS",
+            data : data,
+            msg : "모든 투표 리스트 및 투표율"
         });
     }
 });
