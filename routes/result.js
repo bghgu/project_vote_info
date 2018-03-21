b2a3a1d6109a91c1c346e35e071d71024fd255f5
daseal bae <ektmf1993@gmail.com>
2018-03-21 15:27:50 +0900

    fin

--- routes/result.js
+++ routes/result.js
@@ -6,11 +6,12 @@ const db = require('../module/pool.js');
 router.get('/', async(req, res, next) => {
     const query1 = 'select v.voteId, v.voteName from VOTEINFO v;';
     let voteInfo = await db.execute(query1);
+    let list = []
+    const winner = "당선";
     if(voteInfo.length != 0) {
         for(i = 0; i < voteInfo.length; i++) {
             query2 = 'select b.candidateId, count(*), c.campName, c.leaderName, c.leaderDeptName, c.subLeaderName, c.subLeaderDeptName, c.photo from BLOCKCHAIN b join CANDIDATE c on b.candidateId = c.candidateId where b.voteId = ? group by b.candidateId order by count(*) desc;'
             let data = await db.execute(query2, voteInfo[i].voteId);
-            data[0].message = "당선";
             voteInfo[i].data = data;
         }
         res.status(200).send({
